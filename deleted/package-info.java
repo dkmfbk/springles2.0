bd2b7b4d68c2181855c9e0215fea922f564634fc
gaetanocalabrese <calabrese@fbk.eu>
2016-02-22 12:15:51 +0100

    removed deleted dir

--- deleted/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Implementation package providing data objects and encoding/decoding methods related to the
- * client/server protocol.
- */
-package eu.fbk.dkm.internal.springles.protocol;
-
